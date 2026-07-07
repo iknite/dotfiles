0bc9b28b577d9341ab47bf7af4e5a7e8f7c0e907
iknite <enrique@iknite.com>
2026-07-07 12:27:10 +0200

    feat(nvim): add easy escape and pi-coding-agent

---
+++ .config/nvim/lua/plugins/pi.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "pablopunk/pi.nvim",
+    keys = {
+      { "<leader>ai", ":PiAsk<CR>", desc = "Ask pi" },
+      { "<leader>aI", ":PiAskSelection<CR>", mode = "v", desc = "Ask pi (selection)" },
+    },
+  },
+}
